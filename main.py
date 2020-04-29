2f998718bfcb239b6ebd2264787e1babd0ec1a6b
Shiho <shihohohohohohoho@gmail.com>
2020-04-29 19:41:38 +0800

    New Updates Pics

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import time
 from urllib.request import urlopen
+import sys
 
 
 
@@ -43,20 +44,22 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.variable_maker = {}
+        self.imageDict = {}
+        self.titleDict = {}
+
         x_pos = -50
 
         for x in range(6):
             x_pos += 110
-            self.variable_maker["self_label" + str(x)] = QtWidgets.QLabel(self.centralwidget)
+            self.imageDict["self_label" + str(x)] = QtWidgets.QLabel(self.centralwidget)
 
-            self.variable_maker["self_label" + str(x)].setGeometry(QtCore.QRect(x_pos,MainWindow.frameGeometry().height() - 200 , 100, 150))
-            self.variable_maker["self_label" + str(x)].setText("")
+            self.imageDict["self_label" + str(x)].setGeometry(QtCore.QRect(x_pos,MainWindow.frameGeometry().height() - 200 , 100, 150))
+            self.imageDict["self_label" + str(x)].setText("")
             img = QImage()
             data = urlopen(self.img[x]).read()
             img.loadFromData(data)
             img = img.scaled(100,150)
-            self.variable_maker["self_label" + str(x)].setPixmap(QPixmap(img))
+            self.imageDict["self_label" + str(x)].setPixmap(QPixmap(img))
 
 
         MainWindow.setCentralWidget(self.centralwidget)
@@ -78,42 +81,11 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
 
 
-'''
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 310, 100, 150))
-        self.label.setText("")
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(130, 310, 100, 150))
-        self.label_2.setText("")
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(350, 310, 100, 150))
-        self.label_3.setText("")
-        self.label_3.setObjectName("label_3")
-        self.label_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(240, 310, 100, 150))
-        self.label_5.setText("")
-        self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.centralwidget)
-        self.label_6.setGeometry(QtCore.QRect(460, 310, 100, 150))
-        self.label_6.setText("")
-        self.label_6.setObjectName("label_6")
-        self.label_7 = QtWidgets.QLabel(self.centralwidget)
-        self.label_7.setGeometry(QtCore.QRect(570, 310, 100, 150))
-        self.label_7.setText("")
-        self.label_7.setObjectName("label_7")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(140, 80, 100, 150))
-        self.label_4.setText("")
-        '''
