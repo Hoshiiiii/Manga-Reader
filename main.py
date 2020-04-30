ff4e7558ea68bbeaded06c6271ce6835ba92ac87
Shiho <shihohohohohohoho@gmail.com>
2020-04-30 13:21:17 +0800

    added scrollbar

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap, QImage
 
+from PyQt5.QtWidgets import QApplication, QWidget, QScrollArea, QVBoxLayout, QGroupBox, QLabel, QPushButton, QFormLayout
+import sys
 
 from bs4 import BeautifulSoup as bs
 import requests
 import time
 from urllib.request import urlopen
-import sys
+import ctypes
 
+start = time.time()
 
+class Window(QWidget):
 
-class Ui_MainWindow(object):
-   def __init__(self):
-      self.start = time.time()
-
+    def slut_start(self):
+      self.user = ctypes.windll.user32
 
       self.url = "https://mangadex.org/updates"
       self.content = requests.get(self.url)
@@ -32,99 +34,63 @@ class Ui_MainWindow(object):
           if cnt["src"][:13] == "/images/manga":
               self.img.append("https://mangadex.org" + cnt["src"])
 
-
-
-
-   def setupUi(self, MainWindow):
-
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1280, 860)
-        MainWindow.setStyleSheet("background-color: rgb(66, 70, 88)\n"
-"")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+              
+    def __init__(self):
+        super().__init__()
+        self.slut_start()
+        
+        self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
-
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.setWindowTitle("Manga Reader")
+        self.resize(self.user.GetSystemMetrics(0), self.user.GetSystemMetrics(1))
+        self.setStyleSheet("background-color: rgb(255, 255, 255)\n")
+        
+        formLayout =QFormLayout()
+        groupBox = QGroupBox("")
+        
         self.imageDict = {}
         self.titleDict = {}
 
         x_pos = 0
         y_pos = 0
-        x_pos2 = 400
-        y_pos2 = 0
-        for x in range(8):
-            if x >= 4:
-                self.imageDict["self_label" + str(x)] = QtWidgets.QLabel(self.centralwidget)
+        imgList = []
+        titleList = []
+        F = QtWidgets.QLabel(self.centralwidget)
 
-                self.imageDict["self_label" + str(x)].setGeometry(QtCore.QRect(x_pos2,y_pos2 , 100, 150))
-                self.imageDict["self_label" + str(x)].setText("")
-                img = QImage()
-                data = urlopen(self.img[x]).read()
-                img.loadFromData(data)
-                img = img.scaled(100,150)
-                self.imageDict["self_label" + str(x)].setPixmap(QPixmap(img))
+        for i in  range(len(self.img)):
+            F = QtWidgets.QLabel(self.centralwidget)
+                
+            F.setGeometry(QtCore.QRect(x_pos,y_pos , 100, 150))
+            img = QImage()
+            data = urlopen(self.img[i]).read()
+            img.loadFromData(data)
+            img = img.scaled(100,150)
+            
+            F.setPixmap(QPixmap(img))
+                
+            titleList.append(QLabel(self.title[i]))
+            imgList.append(F)
+            formLayout.addRow(imgList[i],titleList[i])
+            y_pos+=100
+        
 
-                #Title Dictionary
-                self.titleDict["self_label" + str(x)] = QtWidgets.QLabel(self.centralwidget)
-                self.titleDict["self_label" + str(x)].setGeometry(QtCore.QRect(x_pos2+100, y_pos2, 101, 31))
-                font = QtGui.QFont()
-                font.setFamily("Miriam Fixed")
-                font.setPointSize(16)
-                font.setBold(False)
-                font.setWeight(50)
-                self.titleDict["self_label" + str(x)].setFont(font)
-                self.titleDict["self_label" + str(x)].setObjectName("label_2")
-                y_pos2 += 150
-            else:
-                #Image Dictionary
-                self.imageDict["self_label" + str(x)] = QtWidgets.QLabel(self.centralwidget)
+            
+        groupBox.setLayout(formLayout)
+        scroll = QScrollArea()
+        scroll.setWidget(groupBox)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(self.frameGeometry().height())
+        scroll.setFixedWidth(self.frameGeometry().width() - 20)
 
-                self.imageDict["self_label" + str(x)].setGeometry(QtCore.QRect(x_pos,y_pos , 100, 150))
-                self.imageDict["self_label" + str(x)].setText("")
-                img = QImage()
-                data = urlopen(self.img[x]).read()
-                img.loadFromData(data)
-                img = img.scaled(100,150)
-                self.imageDict["self_label" + str(x)].setPixmap(QPixmap(img))
+        layout = QVBoxLayout(self)
+        layout.addWidget(scroll)
 
-                #Title Dictionary
-                self.titleDict["self_label" + str(x)] = QtWidgets.QLabel(self.centralwidget)
-                self.titleDict["self_label" + str(x)].setGeometry(QtCore.QRect(x_pos+100, y_pos, 101, 31))
-                font = QtGui.QFont()
-                font.setFamily("Miriam Fixed")
-                font.setPointSize(16)
-                font.setBold(False)
-                font.setWeight(50)
-                self.titleDict["self_label" + str(x)].setFont(font)
-                self.titleDict["self_label" + str(x)].setObjectName("label_2")
+        self.show()
+        
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
+end = time.time()
+print(end-start)
 
-                y_pos += 150
-
-
-
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 748, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-        self.end = time.time()
-        print(self.end-self.start)
-
-   def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        for x in range(8):
-            self.titleDict["self_label" + str(x)].setText(_translate("MainWindow", self.title[x]))
-
-app = QtWidgets.QApplication(sys.argv)
-MainWindow = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(MainWindow)
-MainWindow.show()
-sys.exit(app.exec_())
