fc77922e68f261d957c20f849ed5f8735e517a9b
Shiho <shihohohohohohoho@gmail.com>
2020-05-10 00:56:32 +0800

    added latest chapter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap, QImage, QPalette, QBrush
+from PyQt5.QtCore import QSize
 
 from PyQt5.QtWidgets import QApplication, QWidget, QScrollArea, QVBoxLayout, QGroupBox, QLabel, QPushButton, QFormLayout
 import sys
@@ -24,73 +25,76 @@ class Window(QWidget):
       self.title = []
       self.site = []
       self.img = []
+      self.chaps = []
+      soupConc = ["a",{"class":"manga_title text-truncate"},'img',]
 
-
-      for cnt in self.soup.find_all("a",attrs={"class":"manga_title text-truncate"}):
-          self.title.append(cnt["title"])
-          self.site.append("https://mangadex.org" + cnt["href"])
-
-      for cnt in self.soup.find_all("img"):
-          if cnt["src"][:13] == "/images/manga":
+      for cnt in self.soup.find_all(soupConc):
+          if cnt.has_attr('class') and "text-truncate" in cnt['class']:
+              if len(cnt['class']) > 1 and cnt['class'][0] == 'manga_title':
+                  self.title.append(cnt["title"] + "\n")
+                  self.site.append("https://mangadex.org" + cnt["href"])
+              else:
+                  self.chaps.append(cnt.text)
+          elif cnt.has_attr('alt') and cnt["src"][:13] == "/images/manga":  
               self.img.append("https://mangadex.org" + cnt["src"])
+      
 
-              
+      for x in range(len(self.title)):
+          self.title[x]+= self.chaps[x]
+
+
     def __init__(self):
         super().__init__()
         self.slut_start()
-        
+
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle("Manga Reader")
         self.resize(self.user.GetSystemMetrics(0), self.user.GetSystemMetrics(1))
-        self.setStyleSheet("background-color: rgb(255, 255, 255)\n")
-        
-        formLayout =QFormLayout()
-        groupBox = QGroupBox("")
-        
-        self.imageDict = {}
-        self.titleDict = {}
+        #self.setStyleSheet("background-image: url(sample.jpg)")
+
 
         x_pos = 0
         y_pos = 0
         imgList = []
         titleList = []
+        formLayout =QFormLayout()
+        groupBox = QGroupBox("")
         F = QtWidgets.QLabel(self.centralwidget)
 
         for i in  range(len(self.img)):
             F = QtWidgets.QLabel(self.centralwidget)
-                
-            F.setGeometry(QtCore.QRect(x_pos,y_pos , 100, 150))
+
             img = QImage()
             data = urlopen(self.img[i]).read()
             img.loadFromData(data)
             img = img.scaled(100,150)
-            
+
             F.setPixmap(QPixmap(img))
-                
+
             titleList.append(QLabel(self.title[i]))
             imgList.append(F)
-            formLayout.addRow(imgList[i],titleList[i])
-            y_pos+=100
-        
 
-            
+            formLayout.addRow(imgList[i],titleList[i])
+
+        titleList[0].mousePressEvent = self.do_smthing
         groupBox.setLayout(formLayout)
         scroll = QScrollArea()
         scroll.setWidget(groupBox)
         scroll.setWidgetResizable(True)
         scroll.setFixedHeight(self.frameGeometry().height())
-        scroll.setFixedWidth(self.frameGeometry().width() - 20)
+        scroll.setFixedWidth(self.frameGeometry().width() /4)
 
         layout = QVBoxLayout(self)
         layout.addWidget(scroll)
+        self.showMaximized()
 
         self.show()
-        
+    def do_smthing(self,event):
+        print("YO")
 App = QApplication(sys.argv)
 window = Window()
-sys.exit(App.exec())
 end = time.time()
 print(end-start)
-
+sys.exit(App.exec())
