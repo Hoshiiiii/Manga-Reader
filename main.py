81ed5e3a61977044b7da7f9d8a874a6bc8a6baa0
Shiho <shihohohohohohoho@gmail.com>
2020-05-10 21:57:12 +0800

    smaller scroll area

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap, QImage, QPalette, QBrush
 from PyQt5.QtCore import QSize
 
-from PyQt5.QtWidgets import QApplication, QWidget, QScrollArea, QVBoxLayout, QGroupBox, QLabel, QPushButton, QFormLayout
+from PyQt5.QtWidgets import QMainWindow,QApplication, QWidget, QScrollArea, QGridLayout, QGroupBox, QLabel, QPushButton, QFormLayout
 import sys
 
 from bs4 import BeautifulSoup as bs
@@ -13,7 +13,7 @@ import ctypes
 
 start = time.time()
 
-class Window(QWidget):
+class Window(QMainWindow):
 
     def slut_start(self):
       self.user = ctypes.windll.user32
@@ -47,52 +47,49 @@ class Window(QWidget):
         super().__init__()
         self.slut_start()
 
+        self.user = ctypes.windll.user32
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle("Manga Reader")
         self.resize(self.user.GetSystemMetrics(0), self.user.GetSystemMetrics(1))
-        #self.setStyleSheet("background-image: url(sample.jpg)")
 
+        
+        layout = QtWidgets.QHBoxLayout(self)
+        scrollArea = QtWidgets.QScrollArea(self)
+        scrollArea.setWidgetResizable(True)
+        scrollAreaWidgetContents = QtWidgets.QWidget()
+        gridLayout = QtWidgets.QGridLayout(scrollAreaWidgetContents)
+        scrollArea.setWidget(scrollAreaWidgetContents)
+        layout.addWidget(scrollArea)
+        layout.setGeometry(QtCore.QRect(0,0,500,800))
 
         x_pos = 0
         y_pos = 0
         imgList = []
         titleList = []
-        formLayout =QFormLayout()
-        groupBox = QGroupBox("")
-        F = QtWidgets.QLabel(self.centralwidget)
 
-        for i in  range(len(self.img)):
+        for i in  range(10):
             F = QtWidgets.QLabel(self.centralwidget)
 
             img = QImage()
             data = urlopen(self.img[i]).read()
             img.loadFromData(data)
             img = img.scaled(100,150)
-
             F.setPixmap(QPixmap(img))
 
             titleList.append(QLabel(self.title[i]))
             imgList.append(F)
 
-            formLayout.addRow(imgList[i],titleList[i])
+            gridLayout.addWidget(F, i, 0)
+            gridLayout.addWidget(titleList[i], i, 1)
 
-        titleList[0].mousePressEvent = self.do_smthing
-        groupBox.setLayout(formLayout)
-        scroll = QScrollArea()
-        scroll.setWidget(groupBox)
-        scroll.setWidgetResizable(True)
-        scroll.setFixedHeight(self.frameGeometry().height())
-        scroll.setFixedWidth(self.frameGeometry().width() /4)
 
-        layout = QVBoxLayout(self)
-        layout.addWidget(scroll)
+        self.setCentralWidget(self.centralwidget)
         self.showMaximized()
-
         self.show()
-    def do_smthing(self,event):
-        print("YO")
+        
+
 App = QApplication(sys.argv)
 window = Window()
 end = time.time()
