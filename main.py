c6a9e265743c953f59bbce012174d31eb4714a04
Shiho <shihohohohohohoho@gmail.com>
2020-05-15 18:44:03 +0800

    add title wtf sir no stretch but working

--- main.py
+++ main.py
@@ -29,19 +29,19 @@ class Window(QMainWindow):
       for cnt in self.soup.find_all(soupConc):
           if cnt.has_attr('class') and "text-truncate" in cnt['class']:
               if len(cnt['class']) > 1 and cnt['class'][0] == 'manga_title':
-                  self.title.append(cnt["title"] + "\n")
                   self.site.append("https://mangadex.org" + cnt["href"])
-              else:
-                  self.chaps.append(cnt.text)
-          elif cnt.has_attr('alt') and cnt["src"][:13] == "/images/manga":  
+                  '''if len(cnt["title"]) > 20:
+                      cnt["title"] = cnt["title"][0:20] + "\n" + cnt["title"][21:len(cnt["title"])]
+                      print(cnt["title"])
+                      continue'''
+                  self.title.append(cnt["title"] + "\n")
+          elif cnt.has_attr('alt') and cnt["src"][:13] == "/images/manga":
               self.img.append("https://mangadex.org" + cnt["src"])
-      
 
-      for x in range(len(self.title)):
-          self.title[x]+= self.chaps[x]
 
-                       
-        
+
+
+
     def __init__(self):
         super().__init__()
         self.slut_start()
@@ -54,23 +54,26 @@ class Window(QMainWindow):
         self.resize(self.user.GetSystemMetrics(0), self.user.GetSystemMetrics(1))
         self.main_label1 = QtWidgets.QLabel(self.centralwidget)
         self.main_label2 = QtWidgets.QLabel(self.centralwidget)
-        self.summary_button = QtWidgets.QPushButton(self.centralwidget)
-
+        self.summary_button = QtWidgets.QLabel(self.centralwidget)
+        self.chapList_button = QtWidgets.QLabel(self.centralwidget)
         layout = QtWidgets.QHBoxLayout(self)
         scrollArea = QtWidgets.QScrollArea(self)
         scrollArea.setWidgetResizable(True)
         scrollAreaWidgetContents = QtWidgets.QWidget()
         gridLayout = QtWidgets.QGridLayout(scrollAreaWidgetContents)
-        scrollArea.setWidget(scrollAreaWidgetContents)
-        layout.addWidget(scrollArea)
-        layout.setGeometry(QtCore.QRect(0,0,int(self.frameGeometry().width() /4),self.frameGeometry().height()))
         
-        x_pos = 0
-        y_pos = 0
+
+        scrollArea.setWidget(scrollAreaWidgetContents)
+        self.gridLayout2 = QtWidgets.QGridLayout(scrollAreaWidgetContents)
+        layout.addWidget(scrollArea)
+        layout.setGeometry(QtCore.QRect(0,0,int(self.frameGeometry().width()),self.frameGeometry().height()))
         imgList = []
         titleList = []
-
-        for i in  range(10):
+        title_vCounter = 0
+        title_hCounter = 0
+        photo_vCounter = 1
+        photo_hCounter = 0
+        for i in  range(20):
             F = QtWidgets.QLabel(self.centralwidget)
 
             img = QImage()
@@ -78,12 +81,23 @@ class Window(QMainWindow):
             img.loadFromData(data)
             img = img.scaled(100,150)
             F.setPixmap(QPixmap(img))
-        
+
             titleList.append(QLabel(self.title[i]))
             imgList.append(F)
-            gridLayout.addWidget(titleList[i], i, 1)
-            gridLayout.addWidget(F, i, 0)
-            titleList[i].mousePressEvent = functools.partial(self.label_events, site=self.site[i])
+
+            if title_hCounter >= 4:
+                title_vCounter += 2
+                photo_vCounter += 2
+                title_hCounter , photo_hCounter = 0,0
+                
+            gridLayout.addWidget(titleList[i],title_vCounter,title_hCounter)
+
+            gridLayout.addWidget(F, photo_vCounter, photo_hCounter)
+
+            title_hCounter +=1
+            photo_hCounter += 1
+
+            #titleList[i].mousePressEvent = functools.partial(self.label_events, site=self.site[i])
 
         self.setCentralWidget(self.centralwidget)
         self.showMaximized()
@@ -95,18 +109,27 @@ class Window(QMainWindow):
         title = soup.find("meta",  property="og:title")
         desc = soup.find("meta",  property="og:description")
         image = soup.find("img",{"class":"rounded"})
-        
+
+        '''
         img = QImage()
         data = urlopen(image["src"]).read()
         img.loadFromData(data)
         img = img.scaled(600,400)
-        
-        self.main_label2.setPixmap(QPixmap(img).copy())
+        self.main_label2.setPixmap(QPixmap(img))
         self.main_label2.setGeometry(QtCore.QRect(int(self.width()/4),0,600,400))
-        
-        self.summary_button.setText("View Summary")
-        self.summary_button.setGeometry(QtCore.QRect(int(self.width()/4),400,100,50))
+        '''
+        pixmap = QPixmap('sum.png')
+        self.summary_button.setPixmap(pixmap)
+        self.summary_button.setGeometry(QtCore.QRect(int(self.width()/4)+10,0,pixmap.height(),pixmap.width()+10))
         self.summary_button.mousePressEvent = functools.partial(self.popup_message, message=desc["content"])
+
+        self.buttonMaker(pixmap,self.chapList)
+    def buttonMaker(self,image_name,button_name,):
+        pixmap = QPixmap(image_name)
+        button_name.setPixmap(pixmap)
+        button_name.setGeometry(QtCore.QRect(int(self.width()/4),400,pixmap.width(),pixmap.height()))
+        #button_name.mousePressEvent = functools.partial(self.popup_message, message=desc["content"])
+
 
     def popup_message(self,event,message):
         msg = QMessageBox()
@@ -114,8 +137,8 @@ class Window(QMainWindow):
 
         msg.setText(' '.join(message.split()))
         x = msg.exec_()  # this will show our messagebox
-        
-        
+
+
 
 
 App = QApplication(sys.argv)
