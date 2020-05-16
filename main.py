1d77e19ccd3e6e7a35a5738c40b9ec3af6495c3c
Shiho <shihohohohohohoho@gmail.com>
2020-05-16 18:26:18 +0800

    searchbox

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap, QImage, QPalette, QBrush
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QMainWindow,QApplication, QWidget, QScrollArea, QVBoxLayout,QStackedWidget,QGridLayout, QGroupBox, QLabel, QPushButton, QFormLayout,QToolBox,QMessageBox,QTabWidget
+from PyQt5.QtWidgets import QMainWindow,QApplication, QWidget, QScrollArea, QVBoxLayout,QStackedWidget,QLineEdit,QGridLayout, QGroupBox, QLabel, QPushButton, QFormLayout,QToolBox,QMessageBox,QTabWidget
 import sys
 import textwrap
 from bs4 import BeautifulSoup as bs
@@ -33,11 +33,13 @@ class Window(QMainWindow):
       for cnt in soup.find_all("a",{"class": "tooltip"}):
         if cnt.img != None:
             self.img.append(cnt.img["src"])
-            if len(cnt.img["alt"]) < 10:
+            if len(cnt.img["alt"]) < 18:
                 self.title.append(cnt.img["alt"])
                 continue
-            #newTitle = cnt.img["alt"][0:10] + "\n" + cnt.img["alt"][10:len(cnt.img["alt"])]
-            newTitle = cnt.img["alt"][0:10]
+            elif " " in cnt.img["alt"][17:len(cnt.img["alt"])]:
+                index = cnt.img["alt"][17:len(cnt.img["alt"])].index(" ")
+                #print("index {} on {}".format(index,cnt.img["alt"][17:len(cnt.img["alt"])]))
+            newTitle = cnt.img["alt"][0:17+index] + "\n" + cnt.img["alt"][17+index:len(cnt.img["alt"])]
             self.title.append(newTitle)
 
 
@@ -50,11 +52,14 @@ class Window(QMainWindow):
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle("Manga Reader")
         self.setStyleSheet("background-color: grey;")
+        
         self.resize(self.user.GetSystemMetrics(0), self.user.GetSystemMetrics(1))
         self.main_label1 = QtWidgets.QLabel(self.centralwidget)
         self.main_label2 = QtWidgets.QLabel(self.centralwidget)
         self.summary_button = QtWidgets.QLabel(self.centralwidget)
         self.chapList_button = QtWidgets.QLabel(self.centralwidget)
+
+        
         layout = QtWidgets.QHBoxLayout(self)
         scrollArea = QtWidgets.QScrollArea(self)
         scrollArea.setWidgetResizable(True)
@@ -64,31 +69,32 @@ class Window(QMainWindow):
         scrollArea.setWidget(scrollAreaWidgetContents)
         layout.addWidget(scrollArea)
         layout.setGeometry(QtCore.QRect(0,0,int(self.frameGeometry().width()),self.frameGeometry().height()))
-        imgList = []
-        titleList = []
-        title_vCounter = 0
-        title_hCounter = 0
-        photo_vCounter = 1
-        photo_hCounter = 0
-        gridLayout.adjustSize()
-        for i in  range(10):
+        imgList, titleList = [], []
+        photo_hCounter, title_hCounter = 0,0,
+        title_vCounter = 2
+        photo_vCounter = 3
+        
+        gridLayout.setHorizontalSpacing(20)
+        
+        searchBox = QLineEdit(self)
+        searchBox.resize(280,40)
+        searchBox.setStyleSheet("background-color: white;")
+        gridLayout.addWidget(searchBox,0,0)
+        for i in  range(20):
             F = QtWidgets.QLabel(self.centralwidget)
-            img = QImage()
-            req = Request(self.img[i], headers={'User-Agent': 'Mozilla/5.0'})
-            data = urlopen(req).read()
-            img.loadFromData(data)
-            img = img.scaled(100,150)
+            img = self.loadImage(self.img[i])
             F.setPixmap(QPixmap(img))
 
             label_title = QPushButton(self)
-            label_title.setStyleSheet("QLabel { color : white; }");
-            label_title.adjustSize()
+            label_title.setStyleSheet("QPushButton { background-color: rgba(255, 255, 255, 0); color : white; }");
+            label_title.setFixedSize(100,50)
             titleList.append(label_title)
             imgList.append(F)
 
-            gridLayout.setRowStretch(i,200)
-            
-            if title_hCounter >= 8:
+            gridLayout.setRowStretch(i,100)
+            gridLayout.setColumnStretch(i,100)
+
+            if title_hCounter >= 10:
                 title_vCounter += 2
                 photo_vCounter += 2
                 title_hCounter , photo_hCounter = 0,0
@@ -97,47 +103,29 @@ class Window(QMainWindow):
 
             gridLayout.addWidget(imgList[i], photo_vCounter, photo_hCounter)
 
-            title_hCounter +=1
+            title_hCounter += 1
             photo_hCounter += 1
 
             titleList[i].setText(self.title[i])
-            titleList[i].mousePressEvent = functools.partial(self.label_event2, F=imgList[i])
+            #titleList[i].mousePressEvent = functools.partial(self.label_event2, F=imgList[i])
 
         self.setCentralWidget(self.centralwidget)
         self.showMaximized()
         self.show()
-    def label_event2(self, event, F=None):
-        F.setPixmap(QPixmap(""))
-
-    def label_events(self, event, site=None):
-        url = site
-        content = requests.get(url)
-        soup = bs(content.content,'html.parser')
-        title = soup.find("meta",  property="og:title")
-        desc = soup.find("meta",  property="og:description")
-        image = soup.find("img",{"class":"rounded"})
-
-        '''
-        img = QImage()
-        data = urlopen(image["src"]).read()
-        img.loadFromData(data)
-        img = img.scaled(600,400)
-        self.main_label2.setPixmap(QPixmap(img))
-        self.main_label2.setGeometry(QtCore.QRect(int(self.width()/4),0,600,400))
-        '''
-        pixmap = QPixmap('sum.png')
-        self.summary_button.setPixmap(pixmap)
-        self.summary_button.setGeometry(QtCore.QRect(int(self.width()/4)+10,0,pixmap.height(),pixmap.width()+10))
-        self.summary_button.mousePressEvent = functools.partial(self.popup_message, message=desc["content"])
-
-        self.buttonMaker(pixmap,self.chapList)
-    def buttonMaker(self,image_name,button_name,):
-        pixmap = QPixmap(image_name)
-        button_name.setPixmap(pixmap)
-        button_name.setGeometry(QtCore.QRect(int(self.width()/4),400,pixmap.width(),pixmap.height()))
-        #button_name.mousePressEvent = functools.partial(self.popup_message, message=desc["content"])
-
-
+        
+    def loadImage(self,image_url):
+            img = QImage()
+            req = Request(image_url, headers={'User-Agent': 'Mozilla/5.0'})
+            data = urlopen(req).read()
+            img.loadFromData(data)  
+            img = img.scaled(100,150)
+            return img
+    def localImage(self,source,label,scaleW,scaleH):
+        pixmap = QPixmap(source)
+        label.setFixedSize(scaleW,scaleH)
+        label.setPixmap(pixmap)
+        return label
+        
     def popup_message(self,event,message):
         msg = QMessageBox()
         msg.setWindowTitle("Manga Summary")
